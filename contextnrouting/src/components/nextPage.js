9263a953f5260e49403a58bc124d9f6f1c1b884e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-08 17:15:38 +0530

    back button

--- contextnrouting/src/components/nextPage.js
+++ contextnrouting/src/components/nextPage.js
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 const NextPage = () => {
+    const navi = useNavigate()
     return(
         <>
             <h1>NextPage</h1>
+            <button onClick={()=>navi(-1)}>Back</button>
         </>
     )
 }
