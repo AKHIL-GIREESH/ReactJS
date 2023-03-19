505b7c8aa4e16aea9df758a66e3a387b6abb7f76
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-19 15:02:22 +0530

    Express

--- someNodeStuff/expreeJS/index.js
+++ someNodeStuff/expreeJS/index.js
@@ -1,16 +1,16 @@
 const express = require("express")()
 
-express.get('/',(req,res) => {
-    res.status(200).send("Login")
+express.get("/",(req,res) => {
+    res.status(200).send("<h1>WelcomePage</h1>")
 })
 
-express.get('/home',(req,res) => {
-    res.status(200).send("Landing")
+express.get("/Home",(req,res) => {
+    res.status(200).send("<h2>HomePage</h2>")
 })
 
-express.all('*',(req,res) => {
-    res.status(404).send("Page Not Found")
+express.all("*",(req,res) => {
+    res.status(404).send("<h2>Page Not Found</h2>")
 })
 
 
-express.listen(3000,() => console.log("Running in 3000"))
+express.listen(3000,() => console.log("Server is Up and Running"))
