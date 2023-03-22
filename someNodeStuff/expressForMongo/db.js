bb340a5305f03cafd1bce5a000c257bf17481672
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-22 13:29:17 +0530

    Mongo

---
+++ someNodeStuff/expressForMongo/db.js
@@ -0,0 +1,21 @@
+const {MongoClient} = require("mongodb")
+require("dotenv").config({ path: ".env" })
+
+const connectionURI = process.env.connectionURI
+let dbConnection
+
+module.exports = {
+    connectToDB:(cb) => {
+        MongoClient.connect(connectionURI)
+        .then((client) => {
+            dbConnection = client.db()
+            return cb()
+        })
+        .catch((error) => {
+            console.error(error)
+            return cb()
+        })
+    },
+
+    getDB: (cb) => {}
+}
