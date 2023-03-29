6df5232e11d3ebda3d8e10f3f1d47f6dd06f991d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-29 06:03:05 +0530

    React-Modal

--- react-modal/src/App.js
+++ react-modal/src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import Modal from "./Modal"
 
 function App() {
+
+  const [open,setOpen] = useState(false)
+
+  const DisplayModal = () => {
+    if (open){
+      return(
+        <Modal/>
+      )
+    }
+  }
+
   return (
     <div className="App">
-      
-      Works
+      <button onClick={()=>{setOpen((prev)=>!prev)}}>Open Modal</button>
+      {DisplayModal()}
     </div>
   );
 }
