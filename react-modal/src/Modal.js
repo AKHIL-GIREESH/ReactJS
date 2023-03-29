92aa5852c2a4d0ed045f5aecc7ffb235a33b6691
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-03-29 13:49:32 +0530

    React-Modal

--- react-modal/src/Modal.js
+++ react-modal/src/Modal.js
@@ -5,20 +5,27 @@ const Modal = () => {
     const [open,setOpen] = useState(false)
 
     const ModalWindow = () => {
-        return(
-            <>
-                <div className="Overlay"></div>
-                <div className="Modal">
-                Modal
-                <button onClick={() => setOpen(false)}>Close</button>
-                </div>
-            </>
-        )
+                return(<>
+                    <div className="Overlay"></div>
+                    <div className="Modal">
+                    Modal
+                    <button onClick={() => setOpen(false)}>Close</button>
+                    </div>
+                </>
+                )
+        }
+
+    const generateButtons = () => {
+        let buttons = []
+        for(let i=0;i<10;i++){
+            buttons.push(<button key={i} onClick={()=>{setOpen(true)}}>Open Modal</button>)
+        }
+        return buttons
     }
 
     return (
         <>
-        <button onClick={()=>{setOpen(true)}}>Open Modal</button>
+        {generateButtons()}
         {open && <ModalWindow/>}
     </>
     )
