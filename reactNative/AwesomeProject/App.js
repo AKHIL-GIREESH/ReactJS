96f681aaf1532b3f37ef2382e50ca52f3a30a151
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-02 11:18:53 +0530

    TextBox

--- reactNative/AwesomeProject/App.js
+++ reactNative/AwesomeProject/App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import Place from './components/placeData';
 
 export default function App() {
+
+  const [place, setPlace] = useState("")
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js</Text>
       <StatusBar style="auto" />
-      <Place name="Earth"/>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Search'
+        multiline
+        onChangeText={(currentText) => setPlace(currentText)}
+        value = {place}
+      />
+      <Button
+        title={place || "Enter the Location"}
+        onPress={() => <Place name="Earth"/>} 
+      />
     </View>
   );
 }
@@ -19,4 +33,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput:{
+    borderWidth: 1,
+    borderColor: "#000",
+    margin:8,
+    width:200
+  }
 });
