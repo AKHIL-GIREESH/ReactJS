be7620d7cc0b4d1c19fef486f9275d307cedd2cf
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-09 23:56:59 +0530

    API

--- someNodeStuff/MERN/mern-frontend/src/App.js
+++ someNodeStuff/MERN/mern-frontend/src/App.js
@@ -4,14 +4,13 @@ import {useState} from "react"
 function App() {
 
   const [search,setSearch] = useState("")
-  console.log(search)
 
   return (
     <div className="App">
       <h1>Playing with APIs</h1>
       <p>API+MERN</p>
-      <input type="text" onChange={(e) => setSearch(e.target.value)}></input>
-      <button>Search</button>
+      <input type="text" onChange={({target}) => setSearch(target.value)}></input>
+      <button onClick={() => console.log(search)}>Search</button>
     </div>
   );
 }
