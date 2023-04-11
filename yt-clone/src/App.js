02f82e1053064593d0bd7ffb0257e86c086d6e15
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-11 23:46:37 +0530

    Yt-clone-Header

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -9,8 +9,11 @@ function App() {
   return (
     <div className="App">
       <div className='header'>
+        <button>Snadwitch</button>
+        <button>Logo</button>
         <input placeholder='Search' onChange={({target}) => setSearch(target.value)}></input>
         <button onClick={() => console.log(search)}>Search</button>
+        <button>Profile</button>
       </div>
       <div className='thumbNails'>
         <ThumbNail/>
