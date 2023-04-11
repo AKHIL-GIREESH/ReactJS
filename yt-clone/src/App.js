88174fdc0173015c2205c07de45c905459f6e732
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-11 23:39:03 +0530

    Yt-clone-searchBar

--- yt-clone/src/App.js
+++ yt-clone/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import ThumbNail from "./components/ThumbNail";
+import { useState } from 'react';
 
 function App() {
+
+  const [search,setSearch] = useState("")
+
   return (
     <div className="App">
       <div className='header'>
-        <input placeholder='Search'></input>
+        <input placeholder='Search' onChange={({target}) => setSearch(target.value)}></input>
+        <button onClick={() => console.log(search)}>Search</button>
       </div>
       <div className='thumbNails'>
-        <ThumbNail/>
         <ThumbNail/>
       </div>
     </div>
