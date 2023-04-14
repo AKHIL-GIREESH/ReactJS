9e28cf0d8d9bde2bd76a35b0ac493842704c8f48
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-14 23:21:18 +0530

    yt-layout

--- yt-clone/src/components/SandwitchMenu/SandwitchMenu.js
+++ yt-clone/src/components/SandwitchMenu/SandwitchMenu.js
@@ -1,15 +1,18 @@
 import { NavLink,Outlet } from "react-router-dom"
+import "./SandwitchMenu.css"
 
 const SandwitchMenu = () => {
     return(
-        <div className="Links">
-            <NavLink to="Home">Home</NavLink>
-            <NavLink to="History">History</NavLink>
-            <NavLink to="Liked">Liked</NavLink>
+        <>
+            <div className="SandWitchMenu">
+                <NavLink to="Home">Home</NavLink>
+                <NavLink to="History">History</NavLink>
+                <NavLink to="Liked">Liked</NavLink>
+            </div>
             <div>
                 <Outlet/>
             </div>
-        </div>
+        </>
     )
 }
 
