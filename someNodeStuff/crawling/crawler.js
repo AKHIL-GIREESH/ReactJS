ba13570d5ba5451ebd78d7dece2e15acba5cbde1
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-17 23:01:23 +0530

    crawling

--- someNodeStuff/crawling/crawler.js
+++ someNodeStuff/crawling/crawler.js
@@ -4,8 +4,13 @@ const { JSDOM } = jsdom;
 const main = async () => {
     const resp = await fetch("https://fisat.ac.in/");
     const respHTML = await resp.text()
-    const respHTMLArray = respHTML.split("</marquee>")
-    console.log(respHTMLArray[0])
+    const respHTMLArrayOpening = respHTML.split(`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`)
+    const respHTMLArrayClosing = respHTML.split("</marquee>")
+
+    console.log(respHTMLArrayOpening[0].length)
+    console.log(respHTMLArrayClosing[0].length)
+
+    console.log(respHTMLArrayClosing[0].substring(respHTMLArrayOpening[0].length+`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`.length))
 }
 
 
