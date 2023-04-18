5bb4171c0bf5e81ecf3d09c94b867fa4b44e381d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-04-18 23:04:05 +0530

    crawling

--- someNodeStuff/crawling/crawler.js
+++ someNodeStuff/crawling/crawler.js
@@ -4,13 +4,15 @@ const { JSDOM } = jsdom;
 const main = async () => {
     const resp = await fetch("https://fisat.ac.in/");
     const respHTML = await resp.text()
-    const respHTMLArrayOpening = respHTML.split(`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`)
-    const respHTMLArrayClosing = respHTML.split("</marquee>")
+    const dom = new JSDOM(respHTML)
+    dom.window.document.querySelectorAll("marquee").forEach(item => console.log(item.innerHTML))
+    // const respHTMLArrayOpening = respHTML.split(`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`)
+    // const respHTMLArrayClosing = respHTML.split("</marquee>")
 
-    console.log(respHTMLArrayOpening[0].length)
-    console.log(respHTMLArrayClosing[0].length)
+    // console.log(respHTMLArrayOpening[0].length)
+    // console.log(respHTMLArrayClosing[0].length)
 
-    console.log(respHTMLArrayClosing[0].substring(respHTMLArrayOpening[0].length+`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`.length))
+    // console.log(respHTMLArrayClosing[0].substring(respHTMLArrayOpening[0].length+`<marquee behavior="scroll" direction="left" scrollamount="8" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()">`.length))
 }
 
 
