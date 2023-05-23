ce6946229f546e270e38275eb6e3c50a6ac3dcd9
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-05-23 23:49:45 +0530

    StoreAPI

--- someNodeStuff/StoreAPI/controllers/Products.js
+++ someNodeStuff/StoreAPI/controllers/Products.js
@@ -1,11 +1,11 @@
 const Products = require("../models/product")
 
 const getAllProducts = (req,res) => {
-    res.status(200).send("All Product")
+    res.status(200).send("All Products")
 }
 
 const getAllProductsStatic = (req,res) => {
-    res.status(200).send("All Product Static")
+    res.status(200).send("All Products Static")
 }
 
 module.exports = {getAllProducts,getAllProductsStatic}
