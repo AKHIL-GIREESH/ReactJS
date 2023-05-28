4f52183a75d80fd28e453fb6caaf78c7bc142153
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-05-28 15:18:56 +0530

    storeapi

--- someNodeStuff/StoreAPI/controllers/Products.js
+++ someNodeStuff/StoreAPI/controllers/Products.js
@@ -4,8 +4,14 @@ const getAllProducts = (req,res) => {
     res.status(200).send("All Products")
 }
 
-const getAllProductsStatic = (req,res) => {
-    res.status(200).send("All Products Static")
+const getAllProductsStatic = async (req,res) => {
+    try{
+    const staticProducts = await Products.find({featured:true})
+    console.log(staticProducts)
+    res.status(200).json({staticProducts,hits:staticProducts.length})
+    }catch(err){
+        console.log(err)
+    }
 }
 
 module.exports = {getAllProducts,getAllProductsStatic}
--- someNodeStuff/StoreAPI/index.js
+++ someNodeStuff/StoreAPI/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
+require("express-async-errors")
 const dbConnect = require("./db/dbConnect")
 const ProductRouter = require("./routes/Products")
 
