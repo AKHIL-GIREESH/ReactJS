a3c00262b65bd7517e523571590d227a2a816992
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-01 17:31:02 +0530

    JWT

--- someNodeStuff/JWT/middleware/auth.js
+++ someNodeStuff/JWT/middleware/auth.js
@@ -1,13 +1,21 @@
-const authorization = () => {
+const jwt = require("jsonwebtoken")
+
+const authorization = (req,res) => {
     try{
         const {authorization} = req.headers
         if(!(authorization.startsWith("Bearer"))){
             res.send("No token Provided")
         }
+
+        console.log(process.env.JWT_SECRET)
         const verification = jwt.verify(authorization.split(" ")[1],process.env.JWT_SECRET)
+        console.log(verification)
         req.headers.loggedUser = verification
+        console.log(req.headers.loggedUser)
         next()
     }catch(err){
         res.status(500).send("Something Went Wrong!")
     }
 }
+
+module.exports = authorization
