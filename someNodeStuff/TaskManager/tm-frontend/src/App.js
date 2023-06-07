b299ce4f8647163ec1950d822e88ed2b0bf1e2f2
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-07 22:36:48 +0530

    TM

--- someNodeStuff/TaskManager/tm-frontend/src/App.js
+++ someNodeStuff/TaskManager/tm-frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import {useState} from "react"
 
 function App() {
+
+  const [count,setCount] = useState(0)
+  setTimeout(() => setCount(1) ,5000)
+
   return (
     <div className="App">
       <h1>Task Manager</h1>
@@ -8,9 +13,9 @@ function App() {
         <input type="text"/>
         <button>✔️</button>
       </div>
-      <div className="glassDiv">
+      {count>0 && <div className="glassDiv">
         
-      </div>
+      </div>}
     </div>
   );
 }
