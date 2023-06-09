73e0d74e29bd9ab8c881b4bf81bcccf81bb79942
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-09 09:56:56 +0530

    TM

--- someNodeStuff/TaskManager/tm-frontend/src/App.js
+++ someNodeStuff/TaskManager/tm-frontend/src/App.js
@@ -4,6 +4,7 @@ import {useEffect, useState} from "react"
 function App() {
 
   const [data,setData] = useState()
+  const [taskData,setTaskData] = useState({})
   //setTimeout(() => setCount(1) ,5000)
   useEffect(() => {
     fetch("http://localhost:3001/api/v1/tasks",{ mode: 'cors' })
@@ -16,7 +17,7 @@ function App() {
     return( 
         <div className='newTask' key={element._id}>
           <input type="text" value={element.task}/>
-          <button className='saveButton'>Save</button>
+          {<button className='saveButton' onClick={() => console.log(element)}>Save</button>}
           {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
           <button>🗑️</button>
         </div>
