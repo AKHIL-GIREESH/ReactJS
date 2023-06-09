23c14ac54272d280fe0af7f66e4db8e9c86c0b42
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-09 09:17:51 +0530

    TM

--- someNodeStuff/TaskManager/tm-frontend/src/App.js
+++ someNodeStuff/TaskManager/tm-frontend/src/App.js
@@ -14,8 +14,9 @@ function App() {
 
   let rowList = data?.map(element => {
     return( 
-        <div className='newTask'>
+        <div className='newTask' key={element._id}>
           <input type="text" value={element.task}/>
+          <button className='saveButton'>Save</button>
           {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
           <button>🗑️</button>
         </div>
