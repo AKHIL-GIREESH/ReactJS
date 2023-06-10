89d6e19cb69e1ce933d42b82b4277c08d0c3a7c3
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-10 17:14:33 +0530

    TM

--- someNodeStuff/TaskManager/tm-frontend/src/App.js
+++ someNodeStuff/TaskManager/tm-frontend/src/App.js
@@ -4,7 +4,8 @@ import Tasks from './tasks';
 
 function App() {
 
-  const [data,setData] = useState()
+  const [data,setData] = useState();
+  const [open,setOpen] = useState()
   //setTimeout(() => setCount(1) ,5000)
   useEffect(() => {
     fetch("http://localhost:3001/api/v1/tasks",{ mode: 'cors' })
@@ -18,16 +19,17 @@ function App() {
   // }
 
   let rowList = data?.map(element => {
-    return( 
-        <Tasks element={element}/>
-        // <div className='newTask' key={element._id}>
-        //   <input type="text" value={element.task}/>
-        //   {<button className='saveButton' onClick={modifyTask}>Save</button>}
-        //   {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
-        //   <button>🗑️</button>
-        // </div>
-      )
-    });
+    return(
+      <>
+        {open && <Tasks element={element}/>}
+        <div className='newTask' key={element._id}>
+          <p>{element.task}</p>
+          <button onClick={() => console.log("Works")}>Show</button>
+          {/* {<button className='saveButton' onClick={modifyTask}>Save</button>}
+          {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
+          <button>🗑️</button> */}
+        </div>
+    </>)});
 
   return (
     <div className="App">
--- someNodeStuff/TaskManager/tm-frontend/src/tasks.js
+++ someNodeStuff/TaskManager/tm-frontend/src/tasks.js
@@ -1,6 +1,6 @@
 const Tasks = ({element}) => {
     return(
-        <div className='newTask' key={element._id}>
+        <div className='Task' key={element._id}>
           <input type="text" value={element.task}/>
           {<button className='saveButton' onClick={() => console.log(element)}>Save</button>}
           {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
