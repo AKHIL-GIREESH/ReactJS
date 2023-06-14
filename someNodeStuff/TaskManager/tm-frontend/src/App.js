c2fcb880ad108803e9a4c2ff395e4217204bf594
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-14 22:40:44 +0530

    TM

--- someNodeStuff/TaskManager/tm-frontend/src/App.js
+++ someNodeStuff/TaskManager/tm-frontend/src/App.js
@@ -8,7 +8,6 @@ function App() {
   const [data,setData] = useState();
   const [newTask,setNewTask] = useState("")
   const [renderflag,setRenderFlag] = useState(false)
-  //setTimeout(() => setCount(1) ,5000)
 
   const reRenderFunc = () => {
     setRenderFlag(!renderflag)
@@ -21,43 +20,14 @@ function App() {
     .catch(error => console.log(error))
   },[renderflag])
 
-  // const modifyTask = () => {
-  //   fetch(`http://localhost:3001/api/v1/tasks/${hell}`)
-  // }
-
   let rowList = data?.map(element => {
     return(
         <Tasks element={element} reRenderFunc={reRenderFunc}/>
-    //     {/* // <div className='newTask' key={element._id}>
-    //     //   <input type="text" value={element.task}/>
-    //     //   {<button className='saveButton' onClick={modifyTask}>Save</button>}
-    //     //   {element.condition?<input type="checkbox" checked></input>:<input type="checkbox"></input>}
-    //     //   <button>🗑️</button>
-    //     // </div>
-    //   )
-    // } */}
   )});
 
   const createNewTask = () => {
-    // fetch(`http://localhost:3001/api/v1/tasks`,{
-    //         method:"POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify({
-    //             task:newTask
-    //         })
-    //     }
-    //     )
-    //     .then(resp => resp.json())
-    //     .then(data => console.log(data))
-    //     .then(() => setRenderFlag(!renderflag))
-    //     .then(() => setNewTask(""))
-    //     .catch(err => console.log(err))
-
     CustomAPI("POST","",{task:newTask},reRenderFunc)
     .then(() => setNewTask(""))
-
   }
 
   return (
