de800c91aba861fba22f3c0ee8e05a082439737f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-22 08:39:05 +0530

    JobsAPI

--- someNodeStuff/JobsAPI/controllers/auth.js
+++ someNodeStuff/JobsAPI/controllers/auth.js
@@ -1,12 +1,16 @@
 const User = require("../models/UserSchema")
+const bcrypt = require("bcrypt")
 
 const login = (req,res) => {
-
+    res.status(200).json({"message":"login"})
 }
 
 const signUp = async (req,res) => {
     try{
-        const signUp = await User.create(req.body)
+        const {password} = req.body
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const signUp = await User.create({...req.body,password:hashedPassword})
         res.status(200).json({"data":signUp})
     }catch(err){
         res.status(400).json({err})
