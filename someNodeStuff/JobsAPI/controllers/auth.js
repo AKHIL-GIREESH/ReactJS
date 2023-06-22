1b9835e22e67e6be821c4f5d7c0f04176421f29d
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-06-22 15:26:11 +0530

    JobsAPI

--- someNodeStuff/JobsAPI/controllers/auth.js
+++ someNodeStuff/JobsAPI/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/UserSchema")
-const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const login = (req,res) => {
     res.status(200).json({"message":"login"})
@@ -8,7 +8,8 @@ const login = (req,res) => {
 const signUp = async (req,res) => {
     try{
         const signUp = await User.create(req.body)
-        res.status(200).json({"data":signUp})
+        const token = await jwt.sign({id:signUp._id,email:signUp.email},process.env.JWT_SECRET,{expiresIn:"30d"})
+        res.status(200).json({"data":signUp,"Token":token})
     }catch(err){
         res.status(400).json({err})
     }
--- someNodeStuff/JobsAPI/middlewares/AsyncWrapper.js
+++ someNodeStuff/JobsAPI/middlewares/AsyncWrapper.js
@@ -1,3 +1,5 @@
 const AsyncWrapper = () => {
-    
+
 }
+
+module.exports = AsyncWrapper
