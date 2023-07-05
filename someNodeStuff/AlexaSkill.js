438546c36e57ba8c010b81402d4c7965a74cb726
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-07-06 02:06:27 +0530

    Alexa

--- someNodeStuff/AlexaSkill.js
+++ someNodeStuff/AlexaSkill.js
@@ -1,22 +1,36 @@
 //Calculator
 
+const randomNumber = (max = 10, min = 0) => Math.floor(Math.random() * (max-min) + min)
 
-const randomNumber = (max = 10, min = 0) => {
-    return Math.floor(Math.random() * (max-min) + min)
+const verifyOption = (list) => {
+    let ran = randomNumber(100)
+    return ran in list? verifyOption(list) : ran
 }
 
 class QnA {
     operatorArray = ["+","-","*","/"]
+    optionsArray = []
     constructor(){
         this.param1 = randomNumber()
         this.param2 = randomNumber(this.param1)
         this.operator = this.operatorArray[randomNumber(4)]
     }
+    question(){
+        console.log(`What is ${this.param1} ${this.operator} ${this.param2}?`)
+    }
     answer(){
         console.log(`${this.param1} ${this.operator} ${this.param2} = ${eval(this.param1+this.operator+this.param2)}`)
+    }
+    generateOptions(){
+        this.optionsArray.push(eval(this.param1+this.operator+this.param2))
+        for(let i=0;i<3;i++){
+            this.optionsArray.push(verifyOption(this.optionsArray))
+        }
+        console.log(this.optionsArray)
     }
 }
 
 const newQ = new QnA()
 
 newQ.answer()
+newQ.generateOptions()
