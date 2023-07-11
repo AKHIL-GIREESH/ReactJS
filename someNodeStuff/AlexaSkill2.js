a708bd13ed3d87c013e041151439cdcf3caafcdf
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-07-11 22:29:13 +0530

    Alexa Skill 2

--- someNodeStuff/AlexaSkill2.js
+++ someNodeStuff/AlexaSkill2.js
@@ -1,8 +1,8 @@
 const randomNumber = (max = 10, min = 0) => Math.floor(Math.random() * (max-min) + min)
 
 const verifyOption = (list) => {
-    let ran = randomNumber(100)
-    return ran in list? verifyOption(list) : ran
+    let ran = randomNumber(122,97)
+    return String.fromCharCode(ran) in list? verifyOption(list) : String.fromCharCode(ran)
 }
 
 class findAnimal {
@@ -14,10 +14,19 @@ class findAnimal {
         this.notItem = this.item.replace(this.answer,"_")
     }
 
+    generateOptions(){
+        for(let i=0;i<3;i++){
+            this.optionsArray.push(verifyOption(this.optionsArray))
+        }
+        this.optionsArray.splice(randomNumber(4),0,this.answer)
+    }
+
     printParams(){
         console.log(this.item)
         console.log(this.answer)
         console.log(this.notItem)
+        this.generateOptions()
+        console.log(this.optionsArray)
     }
 }
 
