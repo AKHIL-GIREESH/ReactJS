cd39a2efbfc6defe85684e4f86ba41aa2187ded4
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-09-11 22:48:48 +0530

    ThreeJS

---
+++ ThreeJS/experimental-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+//import * as THREE from "three"
+
+function App() {
+
+  return (
+    <></>
+    // <div className="App">
+    //   Hello World
+    // </div>
+  );
+}
+
+export default App;
---
+++ ThreeJS/experimental-project/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as THREE from "three"
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+console.log(scene,camera,renderer)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
