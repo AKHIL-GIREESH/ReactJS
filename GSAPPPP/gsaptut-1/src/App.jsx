e6294c5bce0cc4f7a2e21b0026e62da8ea5176ef
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-12-26 07:32:21 +0530

    GSAP

---
+++ GSAPPPP/gsaptut-1/src/App.jsx
@@ -0,0 +1,44 @@
+import { createRoot } from 'react-dom/client'
+import { Canvas, useFrame } from '@react-three/fiber'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect,useRef,useState } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+
+function App() {
+  const headingRef = useRef(null)
+//   const meshRef = useRef(null)
+
+//   useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+//   const [hovered, setHover] = useState(false)
+//   const [active, setActive] = useState(false)
+//   return (
+//     <mesh
+//       {...props}
+//       ref={meshRef}
+//       scale={active ? 1.5 : 1}
+//       onClick={(event) => setActive(!active)}
+//       onPointerOver={(event) => setHover(true)}
+//       onPointerOut={(event) => setHover(false)}>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//     </mesh>
+//   )
+// }
+
+  useEffect(() => {
+    gsap.fromTo(headingRef.current,{opacity:0},{opacity:1,duration:5,scrollTrigger:{trigger:headingRef.current,toggleActions:"restart none reverse none"}})
+  },[])
+
+  return (
+    <>
+      <div style={{width:"100vw",height:"100vh"}}></div>
+      <h1 ref={headingRef}>Hello World</h1>
+      <div style={{width:"100vw",height:"100vh"}}></div>
+    </>
+      )
+    }
+export default App
---
+++ GSAPPPP/gsaptut-1/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Canvas } from '@react-three/fiber'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  {/* <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <App position={[-1.2, 0, 0]} />
+    <App position={[1.2, 0, 0]} />
+  </Canvas> */}
+  </React.StrictMode>,
+)
