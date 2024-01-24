9f667f0551e0cd5411b5d7ad935cb3540ebda83f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-24 14:43:40 +0530

    TED

--- JFDemo/ui/src/App.jsx
+++ JFDemo/ui/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <img src="https://i.ytimg.com/vi/oAUebVIb-7s/maxresdefault.jpg" style={{clipPath:"circle(10% at 50% 50%)"}}></img>
       </div>
       <div className='bottom-layer'>
-        <h1 style={{marginTop:"30vh"}}>Page 2</h1>
+        <h1 style={{marginTop:"35vh"}}>Page 2</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
