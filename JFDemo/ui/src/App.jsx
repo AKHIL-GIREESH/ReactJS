bac3e0ba476bca9ff1c6a80a0fcd3916632d07da
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-24 13:00:42 +0530

    TED

--- JFDemo/ui/src/App.jsx
+++ JFDemo/ui/src/App.jsx
@@ -7,20 +7,32 @@ import './App.css'
 function App() {
 
   useEffect(() => {
-
+    gsap.fromTo("img",{
+      opacity:0,
+      scale:0
+    },{
+      opacity:1,
+      scale:10,
+      scrollTrigger:{
+        trigger:"img",
+        start:"80% top%",
+        scrub:true,
+        markers:true,
+        toggleActions:"restart none none none"}
+    })
   },[])
   
   return(
     <>
       <div className='top-layer'>
         <h1>Page 1</h1>
-        <img></img>
+        <img src="https://i.ytimg.com/vi/oAUebVIb-7s/maxresdefault.jpg" style={{clipPath:"circle(10% at 50% 50%)"}}></img>
+      </div>
+      <div className='bottom-layer'>
+        {/* <h1>Page 2</h1> */}
       </div>
       <div className='bottom-layer'>
         <h1>Page 2</h1>
-      </div>
-      <div className='bottom-layer'>
-        <h1>Page 3</h1>
       </div>
     </>
   )
