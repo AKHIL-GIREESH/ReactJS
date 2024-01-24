1a2be8d46f77bb89383520c28f5a3189da3791f3
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-24 09:10:40 +0530

    TED

--- JFDemo/ui/src/App.jsx
+++ JFDemo/ui/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
 import './App.css'
 
 function App() {
 
-  const [open,setOpen] = useState(false)
+  useEffect(() => {
 
-  return (
-    <h1>Frontend</h1>
+  },[])
+  
+  return(
+    <>
+      <div>
+        <h1>Page 1</h1>
+        <img></img>
+      </div>
+      <div>
+        <h1>Page 2</h1>
+      </div>
+    </>
   )
 }
 
