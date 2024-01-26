7145061d5e79bc8e126ff75c908b7fe18b5b87ab
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-26 07:29:15 +0530

    JF

--- JFDemo/backend/Routes/page.js
+++ JFDemo/backend/Routes/page.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const {GetAPage,GetAllPages,CreatePage,UpdatePage,RemovePage} = require("../Controllers/page")
 
-router.route("/").get().post()
-router.route("/:id").get().patch().delete()
+router.route("/").get(GetAllPages).post(CreatePage)
+router.route("/:id").get(GetAPage).patch(UpdatePage).delete(RemovePage)
+
+module.exports = router
--- JFDemo/backend/index.js
+++ JFDemo/backend/index.js
@@ -2,31 +2,34 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const DBConnect = require("./DB/connectDB")
+const PageRouter = require("./Routes/page")
 
-const authenticate = (req,res,next) => {
-    let {name} = req.query
-    if(name === "john"){
-        next()
-    }else{
-        res.send("Unauthorized")
-    }
-}
+// const authenticate = (req,res,next) => {
+//     let {name} = req.query
+//     if(name === "john"){
+//         next()
+//     }else{
+//         res.send("Unauthorized")
+//     }
+// }
 
-app.use(authenticate)
+// app.use(authenticate)
 app.use(express.json())
 
-app.get("/",(req,res) => {
-    res.status(200).send("<h1>Hello</h1>")
+app.get("/",(req,res)=>{
+    res.status(200).send("<h1>Home</h1>")
 })
 
-app.post("/post",(req,res) => {
-    const {name} = req.body
-    if(name === "john"){
-        res.status(200).send(`<h1>Hello ${name}</h1>`)
-    }else{
-        res.send("Something went wraaang")
-    }
-})
+app.use('/api/v1/page', PageRouter)
+
+// app.post("/post",(req,res) => {
+//     const {name} = req.body
+//     if(name === "john"){
+//         res.status(200).send(`<h1>Hello ${name}</h1>`)
+//     }else{
+//         res.send("Something went wraaang")
+//     }
+// })
 
 app.all("*",(req,res) => {
     res.status(404).send("<h1>404 Page not Found</h1>")
@@ -35,7 +38,7 @@ app.all("*",(req,res) => {
 const start = () => {
     DBConnect(process.env.MONGO_URL)
     .then(app.listen(process.env.PORT,() => console.log("Server is up and Running!")))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log("Something went wrong : "+err))
 }
 
 start()
