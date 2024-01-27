e54bfd09120883c0a1b5b39f3aada99e3ca29e2e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-28 05:07:22 +0530

    JWT server

---
+++ someNodeStuff/JWTagain/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+require("dotenv").config()
+const dbConnect = require("./DB/dbConnect")
+const app = express()
+
+
+app.get("/",(req,res) => {
+    res.status(200).send("<h1>HOME</h1>");
+})
+
+
+
+
+const server = () => {
+    dbConnect(process.env.MONGOURL)
+    .then(() => app.listen(process.env.PORT,() => console.log("Server is up and running")))
+    .catch(() => console.log("Something went wrong"))
+}
+
+server()
