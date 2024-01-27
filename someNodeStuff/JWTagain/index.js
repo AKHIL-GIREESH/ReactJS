52805b7feb482b783abbd25258535c18ed107627
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-28 05:21:03 +0530

    JWT routes and controller

--- someNodeStuff/JWTagain/index.js
+++ someNodeStuff/JWTagain/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
 require("dotenv").config()
 const dbConnect = require("./DB/dbConnect")
+const mainRouter = require("./routes/main")
 const app = express()
+
+app.use(express.json())
+app.use("/api/v1",mainRouter)
 
 
 app.get("/",(req,res) => {
