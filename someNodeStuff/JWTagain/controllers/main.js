d8ebe1dfe8bde74db5bc58d4528c7fff4d50b9c9
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-28 06:50:38 +0530

    JWT

--- someNodeStuff/JWTagain/controllers/main.js
+++ someNodeStuff/JWTagain/controllers/main.js
@@ -1,4 +1,6 @@
 const login = (req,res) => {
+    const {username,password} = req.body
+    const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
     res.status(200).send("Login");
 }
 
