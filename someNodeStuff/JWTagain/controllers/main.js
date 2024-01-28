bcac96dbe807fcb142d052ff63afd957bb12f871
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-28 08:17:28 +0530

    JWT

--- someNodeStuff/JWTagain/controllers/main.js
+++ someNodeStuff/JWTagain/controllers/main.js
@@ -1,11 +1,20 @@
+const jwt = require("jsonwebtoken")
+
 const login = (req,res) => {
     const {username,password} = req.body
     const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
-    res.status(200).send("Login");
+    res.status(200).json({Status:"Success",token:token});
 }
 
 const dashboard = (req,res) => {
-    res.status(200).send("Dashboard")
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(" ")[1]
+    try{
+        const user = jwt.verify(token,process.env.JWT_SECRET)
+        res.status(200).json({user})
+    }catch(err){
+        console.log(err)
+    }
 }
 
 module.exports = {login,dashboard}
