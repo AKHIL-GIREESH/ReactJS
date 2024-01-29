26581de96efd1a6c5710d604e46c04dcea582cb2
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-30 05:21:40 +0530

    authUser

--- someNodeStuff/JWTagain/controllers/main.js
+++ someNodeStuff/JWTagain/controllers/main.js
@@ -6,16 +6,18 @@ const signUp = async (req,res) => {
     const {username,password} = req.body
     const salt = await bcrypt.genSalt(10)
     const newPassword = await bcrypt.hash(password,salt)
-    const data = await userModel.create({username,newPassword});
-    const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
-    res.status(200).json({Status:"Success",token:token});
+    const data = await userModel.create({username,password:newPassword});
+    if(data){
+        const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
+        res.status(200).json({Status:"Success",token:token,data:data});
+    }
 }
 
 const login = async (req,res) => {
     const {username,password} = req.body
     const user = await userModel.findOne({username})
     if(user){
-        const passwordValid = await user.comparePassword(password)
+        const passwordValid = bcrypt.compare(password,user.password)//await user.comparePassword(password)
         if(passwordValid){
             const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
             res.status(200).json({Status:"Success",token:token});
