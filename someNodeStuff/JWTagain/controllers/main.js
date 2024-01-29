9194d02182769e7568d1c2c84465062cc56db5f0
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-29 22:31:44 +0530

    authUser

--- someNodeStuff/JWTagain/controllers/main.js
+++ someNodeStuff/JWTagain/controllers/main.js
@@ -1,7 +1,12 @@
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const userModel = require("../models/userModel")
 
-const signUp = (req,res) => {
+const signUp = async (req,res) => {
     const {username,password} = req.body
+    const salt = await bcrypt.genSalt(10)
+    const newPassword = await bcrypt.hash(password,salt)
+    const data = await userModel.create({username,newPassword});
     const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30d'})
     res.status(200).json({Status:"Success",token:token});
 }
--- someNodeStuff/JWTagain/models/userModel.js
+++ someNodeStuff/JWTagain/models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-const { use } = require("../routes/main")
 
 const userSchema = new mongoose.Schema({
     name:{
