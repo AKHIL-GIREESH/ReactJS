61ddc3680ec781ca8e0e02593e3bf9592551fa7f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-30 17:28:09 +0530

    authUser

--- someNodeStuff/JWTagain/controllers/forapp.js
+++ someNodeStuff/JWTagain/controllers/forapp.js
@@ -9,8 +9,14 @@ const getTask = (req,res) => {
     res.status(200).status("Works!")
 }
 
-const createTask = (req,res) => {
-    res.status(200).status("Works!")
+const createTask = async (req,res) => {
+    try{
+        req.body.createdBy = req.user.user_ID
+        const task = await appModel.create(req.body)
+        res.status(200).json({task})
+    }catch(err){
+        console.log(err)
+    }
 }
 
 const updateTask = (req,res) => {
