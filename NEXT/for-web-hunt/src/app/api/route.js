6b27f037184e337b21fba8dfe02db1396e23dc3e
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-01-31 08:46:50 +0530

    Next

--- NEXT/for-web-hunt/src/app/api/route.js
+++ NEXT/for-web-hunt/src/app/api/route.js
@@ -1,3 +1,9 @@
 export async function GET(){
     return Response.json({status:"Success"})
 }
+
+export async function POST(request){
+    let req = await request.json()
+    console.log(req)
+    return Response.json({status:"Success",body:req})
+}
