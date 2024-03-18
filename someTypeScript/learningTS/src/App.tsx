487ce772452ca0bd8d8d217204ba4f9d8acd1a15
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-18 13:04:17 +0530

    TS

--- someTypeScript/learningTS/src/App.tsx
+++ someTypeScript/learningTS/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
 
 function App() {
-  const user: string = "Hello World" //In this case, need not add the type as we are directly initialising the variable and TS can automatically detect the type unlike JS and hence not overusing TS.
-  console.log(user)
+  const word: string = "Hello World" //In this case, need not add the type as we are directly initialising the variable and TS can automatically detect the type unlike JS and hence not overusing TS.
+  console.log(word)
 
 
   //Functions
@@ -20,18 +20,48 @@ function App() {
   console.log(colours)
 
   //Objects
-
-  const objSalary = (): {
+  //Ex1
+  const objSalary = (ob: {
+    name: string,
+    salary: number
+  }): {
     name: string,
     salary: number
   } => {
-    return {
-      name:"idk",
-      salary: 0
-    }
+    return ob
+  }
+  console.log(objSalary({name:"John Doe",salary:100000}))
+
+  //Ex2 ( using type )
+  type User = {
+    name :string,
+    age:number
   }
 
-  objSalary()
+  const objFunc = (ob: User): User => {
+    return ob
+  }
+
+  console.log(objFunc({name:"John Doe 2",age:100}))
+
+  //Ex3
+  type User2 = {
+    readonly _id:string //cannot be modified
+    name :string
+    age:number
+    creditcardDetails?: number //optional
+  }
+
+  const objFunc2 = (ob: User2): User2 => {
+    return ob
+  }
+
+  console.log(objFunc2({_id:"123",name:"John Doe 2",age:100}))
+
+  //Ex4
+  type User3 = User2 & User & { // combining existing types and adding new params along with it
+    isSingle: boolean 
+  }
 
   return (
     <>
