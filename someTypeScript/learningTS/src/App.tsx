625f94df98622f3b399123c2166aac32c73a7d21
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-18 07:02:28 +0530

    TS

--- someTypeScript/learningTS/src/App.tsx
+++ someTypeScript/learningTS/src/App.tsx
@@ -4,8 +4,25 @@ function App() {
   const user: string = "Hello World" //In this case, need not add the type as we are directly initialising the variable and TS can automatically detect the type unlike JS and hence not overusing TS.
   console.log(user)
 
-  const isLoggedIn :boolean = true
-  console.log(isLoggedIn)  
+
+  //Function Method 1
+  //Ex1
+  const increTwo = (val: number, isPassed: boolean = true): number => { //Checks 
+    return isPassed ? val+2 : -1
+  }
+  console.log(increTwo(2))
+
+  //Ex2
+  let colours = ["red","green","blue"]
+  colours = colours.map((colour):string => {
+    return "Primary colour "+colour
+  })
+
+  console.log(colours)
+
+  //Function Method 2
+
+
 
   return (
     <>
