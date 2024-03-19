dfba5c1f0bc51171097b82b40d8f9e4de695398b
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-03-19 13:00:53 +0530

    RQ

---
+++ react-query/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
+
+function App() {
+  const [display,setDisplay] = useState(false)
+
+  const allUsers = useQuery({
+    queryFn:() => fetch("http://localhost:3000/api/v1/").then(resp => resp.json()).catch(err => console.log(err))
+  })
+
+
+  const signUp = useMutation({
+    mutationFn:() => fetch("http://localhost:3000/api/v1/",{method:"POST",body: JSON.stringify({
+      username:"John Doe 2.0",
+      password:"againNotPassword"
+    }),headers: {
+      "Content-Type": "application/json"
+    }}).then(resp => resp.json()).catch(err => console.log(err)),
+    onSuccess: () => console.log("Works"),
+    onError: (error) => console.error('Error during sign-up:', error)
+    })
+
+
+  console.log(allUsers?.data)
+  
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <button onClick={() => signUp.mutate()}>Click</button>
+    </>
+  );
+}
+
+export default App;
--- someTypeScript/learningTS/src/App.tsx
+++ someTypeScript/learningTS/src/App.tsx
@@ -1,80 +1,29 @@
-import './App.css'
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useQuery } from "../node_modules/@tanstack/react-query/build/legacy/useQuery";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
-  const word: string = "Hello World" //In this case, need not add the type as we are directly initialising the variable and TS can automatically detect the type unlike JS and hence not overusing TS.
-  console.log(word)
+  const [display,setDisplay] = useState(false)
 
-
-  //Functions
-  //Ex1
-  const increTwo = (val: number, isPassed: boolean = true): number => { //Checks 
-    return isPassed ? val+2 : -1
-  }
-  console.log(increTwo(2))
-
-  //Ex2
-  let colours = ["red","green","blue"]
-  colours = colours.map((colour):string => {
-    return "Primary colour "+colour
+  const allUsers = useQuery({
+    queryFn:() => fetch("http://localhost:3000/api/v1/")
   })
-  console.log(colours)
 
-  //Objects
-  //Ex1
-  const objSalary = (ob: {
-    name: string,
-    salary: number
-  }): {
-    name: string,
-    salary: number
-  } => {
-    return ob
-  }
-  console.log(objSalary({name:"John Doe",salary:100000}))
-
-  //Ex2 ( using type )
-  type User = {
-    name :string,
-    age:number
-  }
-
-  const objFunc = (ob: User): User => {
-    return ob
-  }
-
-  console.log(objFunc({name:"John Doe 2",age:100}))
-
-  //Ex3
-  type User2 = {
-    readonly _id:string //cannot be modified
-    name :string
-    age:number
-    creditcardDetails?: number //optional
-  }
-
-  const objFunc2 = (ob: User2): User2 => {
-    return ob
-  }
-  console.log(objFunc2({_id:"123",name:"John Doe 2",age:100}))
-
-  //Ex4
-  type User3 = User2 & User & { // combining existing types and adding new params along with it
-    isSingle: boolean 
-  }
-
-  //Array
-  const fruits: Array<string> = []
-  const users: Array<User> = []
-
-  //Union
-  const _user: User | User2 = {_id:"123",name:"John Doe 2",age:100}
-  const data1: (string | number)[] = ["1",2]
-
+  console.log(allUsers)
 
   return (
-    <>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <button onClick={() => setDisplay(state => !state) }>{"Hello"}</button>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
