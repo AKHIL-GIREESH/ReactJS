42b261328786bec8593475e770e24286e16fc03a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-06-25 11:37:03 +0530

    boilerplate

--- someNodeStuff/iceTest/index.js
+++ someNodeStuff/iceTest/index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello"));
+
+app.listen(3001, () => console.log("Server is up and running"));
