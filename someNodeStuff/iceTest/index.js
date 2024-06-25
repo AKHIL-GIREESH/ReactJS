41bfd67bf22429d22ae0f95c8df7b887bed6aa02
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-06-25 12:36:14 +0530

    node done

--- someNodeStuff/iceTest/index.js
+++ someNodeStuff/iceTest/index.js
@@ -1,6 +1,34 @@
 const express = require("express");
+const { google } = require("googleapis");
+require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", async (req, res) => {
+  //   res.send("Hello");
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  const client = await auth.getClient();
+
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = process.env.sheetID;
+
+  //   const metaData = await googleSheets.spreadsheets.get({
+  //     auth,
+  //     spreadsheetId,
+  //   });
+
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Form Responses 1",
+  });
+
+  res.json(getRows.data.values);
+});
 
 app.listen(3001, () => console.log("Server is up and running"));
