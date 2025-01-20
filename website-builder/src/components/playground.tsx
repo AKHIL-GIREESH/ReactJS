282a739154847e9823b897e31248f8166d239a7c
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-01-20 08:27:33 +0530

    draggable

--- website-builder/src/components/playground.tsx
+++ website-builder/src/components/playground.tsx
@@ -6,7 +6,24 @@ const Playground = () => {
     console.log(editorState?.state)
     return(
         <div style={{width:"80vw",height:"100vh"}}>
-            
+            <div style={{display:"flex",border:"1px solid",width:"70vw",marginLeft:"5vw",height:"80vh"}}>
+                <div style={{border:"1px solid",width:"50%",height:"100%"}}>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                </div>
+                <div>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px"}}></div>
+                    <br/>
+                </div>
+            </div>
         </div>
     )
 }
