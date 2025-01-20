2d188d92b58d36fe6485c46eb0109a465b9e7511
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-01-20 23:04:12 +0530

    draggable

--- website-builder/src/components/CardTemp.tsx
+++ website-builder/src/components/CardTemp.tsx
@@ -1,14 +1,19 @@
+import CardArea from "./CardArea"
 import { card } from "./playground"
 
 type Props = {
-    index: Number
+    index: number
     side: Number
     setCard: (val: card | null) => void
+    onDRag: (val: card) => void
 }
 
-const CardTemp = ({index,side,setCard}: Props) => {
+const CardTemp = ({index,side,setCard,onDRag}: Props) => {
   return (
-    <div draggable style={{backgroundColor:"white",height:"50px",width:"50px",marginBottom:"10px"}} onDragStart={() => setCard({index:index,side:side})} onDragEnd={() => setCard(null)}>{""+index}</div>
+    <>
+        <div draggable style={{backgroundColor:"white",height:"50px",width:"50px",marginBottom:"10px"}} onDragStart={() => setCard({index:index,side:side})} onDragEnd={() => setCard(null)}>{""+index}</div>
+        <CardArea index={index+1} side={side} onDRag={onDRag}/>
+    </>
   )
 }
 
--- website-builder/src/components/playground.tsx
+++ website-builder/src/components/playground.tsx
@@ -3,7 +3,7 @@ import { EditorContext } from "../providers/editorProvider"
 import CardTemp from "./CardTemp"
 
 export type card = {
-    index: Number
+    index: number
     side: Number
 }
 
@@ -15,14 +15,18 @@ const Playground = () => {
     const [card, setCard] = useState<null | card>(null)
     const [cards,setCards] = useState<null | card[]>([{index:1,side:1},{index:2,side:1},{index:3,side:1},{index:4,side:2},{index:5,side:2},{index:6,side:2}])
 
+    const onDRag = (val:card) => {
+        console.log(val.index,val.side)
+    }
+
     return(
         <div style={{width:"80vw",height:"100vh"}}>
             <div style={{display:"flex",border:"1px solid",width:"70vw",marginLeft:"5vw",height:"80vh"}}>
                 <div style={{border:"1px solid",width:"50%",height:"100%"}}>
-                    {cards?.map(({index,side}) => side === 1 && <CardTemp key={""+index} index={index} side={side} setCard={setCard}/>)}
+                    {cards?.map(({index,side}) => side === 1 && <CardTemp key={""+index} index={index} side={side} setCard={setCard} onDRag={onDRag}/>)}
                 </div>
                 <div>
-                {cards?.map(({index,side}) => side === 2 && <CardTemp key={""+index} index={index} side={side} setCard={setCard}/>)}
+                {cards?.map(({index,side}) => side === 2 && <CardTemp key={""+index} index={index} side={side} setCard={setCard} onDRag={onDRag}/>)}
                 </div>
             </div>
         </div>
