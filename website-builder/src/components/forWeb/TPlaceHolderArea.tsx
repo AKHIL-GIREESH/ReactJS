c586a9213a3986f60bc2ced49f610a2efa6b2038
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-28 04:25:53 +0530

    useGetStyle hook

--- website-builder/src/components/forWeb/TPlaceHolderArea.tsx
+++ website-builder/src/components/forWeb/TPlaceHolderArea.tsx
@@ -1,40 +1,13 @@
-import { useContext, useState } from "react"
-import { SideBarDrag } from "../../providers/editorProvider"
-import { elementKind } from "../../types/editor"
-
-//type Props = {}
+import { useState } from "react"
+import { useGetStyles } from "../../hooks/getStyles"
 
 const TPlaceHolderArea = ({parent, id}:{parent:string, id:string}) => {
   const [showArea,setShowArea] = useState(false)
 
-  const sideBarSelectionContext = useContext(SideBarDrag)
+  const style = useGetStyles()
 
-  if(sideBarSelectionContext == null){
-    throw new Error("Error")
-  }
-
-  const {state} = sideBarSelectionContext
-
-  const selectStyle = (state:elementKind | null) => {
-    if(state == "Container"){
-      return {border:"1px solid",minHeight:"200px",width:"200px",height:"fit-content",backgroundColor:"white",opacity:"50%",color:"black"}
-    }else if(state == "Elem"){
-      return {backgroundColor:"white",opacity:"50%",minHeight:"20px",width:"155px",color:"black"}
-    }else{
-      return {
-
-      }
-    }
-  }
-
-  // styles = {
-  //   ...styles,
-  //   backgroundColor:"white",
-  //   opacity:"50%",
-  //   color:"black"
-  // }
   return (
-      <div id={id} onDragEnter={() => setShowArea(true)} onDragLeave={() => setShowArea(false)} onDragOver={e => e.preventDefault()} style={showArea?selectStyle(state):{opacity:0}}>
+      <div id={id} onDrop={() => console.log("id:"+id+" parent:"+parent)} onDragEnter={() => setShowArea(true)} onDragLeave={() => setShowArea(false)} onDragOver={e => e.preventDefault()} style={showArea?style:{opacity:0}}>
         Drop Here
       </div>
 
