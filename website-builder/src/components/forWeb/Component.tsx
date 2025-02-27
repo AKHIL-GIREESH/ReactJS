7e48210ff9da7f27c80437efa9fd3d12375b3f0e
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-27 21:12:01 +0530

    added placeholders by dafault inside containers

--- website-builder/src/components/forWeb/Component.tsx
+++ website-builder/src/components/forWeb/Component.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import { EditorContainerType, EditorElementType } from '../../types/editor'
 import TPlaceHolderArea from './TPlaceHolderArea';
 
@@ -7,9 +8,13 @@ type ComponentProps = EditorContainerType & {
 };
 
 const Component: React.FC<ComponentProps> = ({ contents, styles, kind, recFunc, parent , id }) => {
+  
+  const placeHolderArea = uuidv4()
+
   return (
     <>
         <div style={styles}>
+            <TPlaceHolderArea parent={id} id={placeHolderArea} key={id+placeHolderArea}/>
             {contents?.map(item => recFunc(item))}
         </div>
         <TPlaceHolderArea parent={parent} id={id} key={parent+id}/>
