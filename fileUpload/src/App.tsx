1a40e1ca0d1d86069457e5225f030f0330888d36
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-03-19 19:36:57 +0530

    fileupload

--- fileUpload/src/App.tsx
+++ fileUpload/src/App.tsx
@@ -4,13 +4,17 @@ import './App.css'
 function App() {
   const [file, setFile] = useState<File | null>(null)
   const [uploading, setUploading] = useState(false)
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null)
   const [fileUrl, setFileUrl] = useState<string | null>(null) // To store the URL after the upload
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files ? e.target.files[0] : null
     if (selectedFile) {
       setFile(selectedFile)
+      setPreviewUrl(URL.createObjectURL(selectedFile))
     }
+
+
   }
 
   const uploadFile = async () => {
@@ -28,9 +32,6 @@ function App() {
       const response = await fetch('http://localhost:3000/upload', {
         method: 'POST',
         body: formData,
-        headers: {
-
-        }
       })
 
       if (!response.ok) {
@@ -50,7 +51,10 @@ function App() {
 
   return (
     <div className="App">
-      <input type="file" onChange={handleFileChange} />
+      <div style={{ width: 100, height: 100, backgroundColor: "white" }}>
+        <input type="file" onChange={handleFileChange} style={{ position: "absolute", opacity: 0, width: 100, height: 100, }} />
+
+      </div>
       <button onClick={uploadFile} disabled={uploading}>
         {uploading ? 'Uploading...' : 'Upload'}
       </button>
@@ -61,6 +65,10 @@ function App() {
           <p>File uploaded successfully! View it <a href={fileUrl} target="_blank" rel="noopener noreferrer">here</a>.</p>
         </div>
       )}
+      {
+        previewUrl &&
+        <img src={previewUrl} />
+      }
     </div>
   )
 }
